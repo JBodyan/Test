a98bf854cdd73d6090169c2de0b6d69bafd25e2d
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 20:03:51 +0300

    Added db context. Added entity (SomeData). Added config (SomeData).

---
+++ Data/Config/SomeDataConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Config
+{
+    public class SomeDataConfig : IEntityTypeConfiguration<SomeData>
+    {
+        public void Configure(EntityTypeBuilder<SomeData> builder)
+        {
+            builder.ToTable("SomeData");
+
+            builder.Property(x => x.Id)
+                .HasColumnName("id");
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(x => x.Code)
+                .HasColumnName("code")
+                .HasMaxLength(3)
+                .IsRequired();
+
+        }
+    }
+}
---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Data.Config;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<SomeData> SomeData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new SomeDataConfig());
+        }
+    }
+}
