1b807eeac4875440caab970e8556558e318c1f4d
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 20:36:17 +0300

    Added dto classes for SomeData

---
+++ BL/Dto/SomeDataUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL.Dto
+{
+    public class SomeDataUpdateDto
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
