13ab2b9c9528c3dd6b0bcd514f9dc2ab1b510768
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 20:41:00 +0300

    Added mapper. Added profile for SomeData.

---
+++ BL/Mapper/SomeDataProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using BL.Dto;
+using Data.Entities;
+
+namespace BL.Mapper
+{
+    public class SomeDataProfile : Profile
+    {
+        public SomeDataProfile()
+        {
+            CreateMap<SomeDataDto, SomeData>().ReverseMap();
+            CreateMap<SomeDataUpdateDto, SomeData>().ReverseMap();
+            CreateMap<SomeDataAddDto, SomeData>().ReverseMap();
+        }
+    }
+}
