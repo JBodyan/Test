a8f8c5ef6e01d41b4878539c6b4cc3c3474b9378
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 22:33:10 +0300

    Added validators.

---
+++ TestApp/Validators/SomeDataAddValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL.Dto;
+
+namespace TestApp.Validators
+{
+    public class SomeDataAddValidator : AbstractValidator<SomeDataAddDto>
+    {
+        public SomeDataAddValidator()
+        {
+            RuleFor(x => x.Code)
+                .Matches(@"\d{3}")
+                .NotEmpty();
+
+            RuleFor(x => x.Name)
+                .MinimumLength(3)
+                .MaximumLength(20)
+                .NotEmpty();
+        }
+    }
+}
---
+++ TestApp/Validators/SomeDataUpdateValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL.Dto;
+using FluentValidation;
+
+namespace TestApp.Validators
+{
+    public class SomeDataUpdateValidator : AbstractValidator<SomeDataUpdateDto>
+    {
+        public SomeDataUpdateValidator()
+        {
+            RuleFor(x => x.Code)
+                .Matches(@"\d{3}")
+                .NotEmpty();
+
+            RuleFor(x => x.Name)
+                .MinimumLength(3)
+                .MaximumLength(20)
+                .NotEmpty();
+        }
+    }
+}
