093293956dc52135b6355f4616864bc6204eb754
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 20:48:40 +0300

    Updated interface SomeDataService

--- BL/Services/ISomeDataService.cs
+++ BL/Services/ISomeDataService.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BL.Dto;
 
 namespace BL.Services
 {
     public interface ISomeDataService
     {
+        SomeDataDto GetById(int id);
+        List<SomeDataDto> GetAll();
+        SomeDataUpdateDto Update(SomeDataUpdateDto model);
+        SomeDataAddDto Add(SomeDataAddDto model);
+        void Remove(int id);
     }
 }
