64a91d4ad24e8bf12a0d27a92abb4c2504890a6c
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 22:32:48 +0300

    Created filter.

---
+++ TestApp/Filters/SomeDataUpdateFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TestApp.Filters
+{
+    public class SomeDataUpdateFilter : Attribute, IResourceFilter
+    {
+        private readonly int _id;
+        public SomeDataUpdateFilter(int id)
+        {
+            _id = id;
+        }
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+        }
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            context.HttpContext.Response.Cookies.Append("Id", _id.ToString());
+        }
+    }
+}
