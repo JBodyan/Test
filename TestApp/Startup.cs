8ef6e746e83fd257338dfe863d7c389e0efc9581
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 23:03:53 +0300

    Configuration

--- TestApp/Startup.cs
+++ TestApp/Startup.cs
@@ -2,14 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using BL.Mapper;
+using BL.Services;
+using Data;
+using Data.Repository;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TestApp.Validators;
 
 namespace TestApp
 {
@@ -25,6 +33,28 @@ namespace TestApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection"),
+                    x=> x.MigrationsAssembly("TestApp")));
+
+            services.AddSingleton(new MapperConfiguration(config =>
+            {
+                config.AddProfile(new SomeDataProfile());
+            }).CreateMapper());
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            
+            services.AddScoped<ISomeDataService, SomeDataService>();
+
+            services.AddMvc().AddFluentValidation(config =>
+            {
+                config.RegisterValidatorsFromAssemblyContaining<SomeDataAddValidator>();
+                config.RegisterValidatorsFromAssemblyContaining<SomeDataUpdateValidator>();
+            });
+
             services.AddControllers();
         }
 
