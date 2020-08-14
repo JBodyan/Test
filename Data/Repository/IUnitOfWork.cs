ba3dc32b4bd8cafaeef05973df34aa64792fc97d
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 20:31:20 +0300

    Added UoW.

---
+++ Data/Repository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Entities;
+
+namespace Data.Repository
+{
+    public interface IUnitOfWork
+    {
+        IRepository<SomeData> SomeDataRepository { get; }
+        void Commit();
+        void Rollback();
+    }
+}
---
+++ Data/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+
+        private IRepository<SomeData> _someDataRepository;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<SomeData> SomeDataRepository
+        {
+            get { return _someDataRepository ??= new Repository<SomeData>(_context); }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+           _context.Dispose();
+        }
+    }
+}
