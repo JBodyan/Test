3c60685f58bdf3a4b8340bf729190900314ae940
JBodyan <jstix.bogdan.kandela@gmail.com>
2020-08-14 20:30:57 +0300

    Added base repository.

---
+++ Data/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data.Entities;
+
+namespace Data.Repository
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        IQueryable<T> GetAll();
+        T GetById(params object[] keys);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+
+    }
+}
---
+++ Data/Repository/Repository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly DbSet<T> _entities;
+        public Repository(DatabaseContext context)
+        {
+            _entities = context.Set<T>();
+        }
+        public IQueryable<T> GetAll()
+        {
+            return _entities.AsQueryable();
+        }
+
+        public T GetById(params object[] keys)
+        {
+            return _entities.Find(keys);
+        }
+
+        public void Add(T entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _entities.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _entities.Remove(entity);
+        }
+    }
+}
